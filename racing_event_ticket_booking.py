5eef6d4106ee310035af0d1b0cf58332449dff7f
Z402516 <202218796@zu.ac.ae>
2025-05-12 21:05:55 +0500

    Create racing_event_ticket_booking.py
    customer class

---
+++ racing_event_ticket_booking.py
@@ -0,0 +1,72 @@
+import pickle
+
+class Customer:
+    def __init__(self, id, name, email, phone):
+        self.__id = id
+        self.__name = name
+        self.__email = email
+        self.__phone = phone
+        self.__purchase_history = []
+
+    # Hardcore getters
+    def get_id(self):
+        return self.__id
+
+    def get_name(self):
+        return self.__name
+
+    def get_email(self):
+        return self.__email
+
+    def get_phone(self):
+        return self.__phone
+
+    # Minimal setters
+    def set_name(self, new_name):
+        self.__name = new_name
+
+    def set_email(self, new_email):
+        self.__email = new_email
+
+    def set_phone(self, new_phone):
+        self.__phone = new_phone
+
+    # purchase methods
+    def add_purchase(self, ticket):
+        '''
+        Takes the ticket and adds the ticket into the purchased tickets list
+        :param ticket:
+        :return:
+        '''
+        self.__purchase_history.append(ticket)
+
+    def cancel_purchase(self, ticket):
+        '''
+        This method cancels the purchase by removing the ticket from the list
+        :param ticket:
+        :return:
+        '''
+        self.__purchase_history.remove(ticket)
+
+    def save_to_file(self):
+        '''
+        This method adds the customer information into the binary pickle file
+        from which the information can be read whenever required.
+        :return:
+        '''
+        try:
+            with open("customers.pkl", "rb") as file:
+                customers = pickle.load(file)
+        except (FileNotFoundError, EOFError):
+            customers = []
+
+        customers.append(self)
+
+        with open("customers.pkl", "wb") as file:
+            pickle.dump(customers, file)
+
+    def __str__(self):
+        return "*** Showing Details for Customer ***\nID : {}\nName : {}\n" \
+               "Email : {}\nPhone : {}\nTotal Orders : {}".format(self.get_id(),self.get_name(),
+                                                                  self.get_email(),self.get_phone(),
+                                                                  len(self.__purchase_history))
